23d9e5a3f0d2f263deda9fb96ae8ba7970a24438
yann-lcrx <yannick84v@gmail.com>
2022-09-18 21:51:28 +0200

    feat: fetch data from all endpoints

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -2,20 +2,42 @@ import useFetch from "../services/fetch";
 import styles from "../styles/dashboard.module.scss";
 
 function Dashboard() {
-  const { data, loading, error } = useFetch("user/12");
+  const {
+    data: personalData,
+    loading: personalDataIsLoading,
+    error: personalDataError,
+  } = useFetch("user/12");
+
+  const {
+    data: activityData,
+    loading: activityDataIsLoading,
+    error: activityDataError,
+  } = useFetch("user/12/activity");
+
+  const {
+    data: averageSessionData,
+    loading: averageSessionDataIsLoading,
+    error: averageSessionDataError,
+  } = useFetch("user/12/average-sessions");
+
+  const {
+    data: performanceData,
+    loading: performanceDataIsLoading,
+    error: performanceDataError,
+  } = useFetch("user/12/performance");
 
   return (
     <main className={styles.Dashboard}>
-      {loading ? (
+      {personalDataIsLoading ? (
         <p>Chargement</p>
       ) : (
         <>
-          {data &&
-            (error ? (
-              <p>{data}</p>
+          {personalData &&
+            (personalDataError ? (
+              <p>{personalData}</p>
             ) : (
               <h1>
-                Bonjour <span>{data.userInfos.firstName}</span>
+                Bonjour <span>{personalData.userInfos.firstName}</span>
               </h1>
             ))}
         </>
