097c30067a08d7bfa7ff89b65e26b376bf3cf543
yann-lcrx <yannick84v@gmail.com>
2022-09-18 21:39:48 +0200

    styles: add base font and variables

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,19 +1,26 @@
 import useFetch from "../services/fetch";
+import styles from "../styles/dashboard.module.scss";
 
 function Dashboard() {
   const { data, loading, error } = useFetch("user/12");
 
   return (
-    <>
+    <main className={styles.Dashboard}>
       {loading ? (
         <p>Chargement</p>
       ) : (
         <>
           {data &&
-            (error ? <p>{data}</p> : <p>Bonjour {data.userInfos.firstName}</p>)}
+            (error ? (
+              <p>{data}</p>
+            ) : (
+              <h1>
+                Bonjour <span>{data.userInfos.firstName}</span>
+              </h1>
+            ))}
         </>
       )}
-    </>
+    </main>
   );
 }
 
