48c6c6d935ecda99387c63aecfe7b56f7b0bf4eb
yann-lcrx <yannick84v@gmail.com>
2022-09-25 21:36:42 +0200

    feat: add proptypes and bar graph styles

--- src/components/barChart.jsx
+++ src/components/barChart.jsx
@@ -3,6 +3,7 @@ import {
   BarChart,
   CartesianGrid,
   Legend,
+  Text,
   Tooltip,
   XAxis,
   YAxis,
@@ -13,14 +14,26 @@ function BarDataChart({ data, data1, data2 }) {
     <BarChart width={835} height={320} barSize={7} data={data}>
       <CartesianGrid strokeDasharray="3 3" vertical={false} />
       <XAxis dataKey="day" />
-      <YAxis orientation="right" tickCount={3} />
-      <Tooltip />
+      <YAxis orientation="right" tickCount={3} color={"fff"} axisLine={false} />
+      <Tooltip
+        contentStyle={{
+          color: "white",
+          background: "#E60000",
+          fontSize: "7px",
+        }}
+        labelStyle={{
+          display: "none",
+        }}
+        itemStyle={{ color: "white", fontSize: "7px" }}
+        wrapperStyle={{ fontSize: "7px" }}
+      />
       <Legend
         verticalAlign="top"
         align="right"
         iconType="circle"
         color="#7479BC"
       />
+      <Text>Activité quotidienne</Text>
       <Bar dataKey={data1.key} fill={data1.color} />
       <Bar dataKey={data2.key} fill={data2.color} />
     </BarChart>
